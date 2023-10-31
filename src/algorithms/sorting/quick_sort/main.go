9bc9cf9d9e3d4506bcd61e4c610d549140ad203d
eduardor2m <deveduardomelo@gmail.com>
2023-10-31 17:09:54 -0300

    :tada: feat: base

--- src/algorithms/sorting/quick_sort/main.go
+++ src/algorithms/sorting/quick_sort/main.go
@@ -1,1 +1,31 @@
 package main
+
+import "fmt"
+
+func QuickSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	pivot := arr[0]
+	var left, right []int
+
+	for _, element := range arr[1:] {
+		if element <= pivot {
+			left = append(left, element)
+		} else {
+			right = append(right, element)
+		}
+	}
+
+	left = QuickSort(left)
+	right = QuickSort(right)
+
+	return append(append(left, pivot), right...)
+}
+
+func main() {
+	arr := []int{3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5}
+	sortedArr := QuickSort(arr)
+	fmt.Println(sortedArr)
+}
